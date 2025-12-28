fd3bce654b08c20b268219a9ddec4cf74eefc326
Nour Lababidi <nourzymta@gmail.com>
2025-12-29 01:25:17 +0200

    Update something

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,1 +1,7 @@
-   
+   export default function AboutUsPage() {
+  return (
+    <section className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold">About Us</h1>
+    </section>
+  );
+}
---
+++ src/app/find-us/page.js
@@ -0,0 +1,7 @@
+export default function FindUsPage() {
+  return (
+    <section className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold">Find Us</h1>
+    </section>
+  );
+}
